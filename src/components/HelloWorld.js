e6ed33930487a67d5dbe276a1fd2b2b9a6472fb4
cary <fzhao321@foxmail.com>
2016-08-03 00:07:14 +0800

    first commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+export default class HelloWorld extends React.Component {
+  render() {
+    return (
+      <Button>你好</Button>
+    );
+  }
+}
